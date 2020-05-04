bbe3aaa4b10b78e0c71bbea4f135be12edd16097
Elkypoo <41938455+Elkypoo@users.noreply.github.com>
2020-05-03 22:37:14 -0700

    a

--- script.js
+++ script.js
@@ -6,13 +6,19 @@ var vmin = Math.min(vw, vh);
 function scrollFunction() {
   var startThreshold = 12;
   var endThreshold = 35;
-  console.log(document.body.scrollTop / vh);
+  // Header grow on scroll
   if (Math.max(document.body.scrollTop, document.documentElement.scrollTop) > endThreshold * vh) {
     document.getElementById("header").style.fontSize = "" + endThreshold * vh / 2;
   } else if (Math.max(document.body.scrollTop, document.documentElement.scrollTop) < endThreshold * vh && Math.max(document.body.scrollTop, document.documentElement.scrollTop) > startThreshold * vh){
     document.getElementById("header").style.fontSize = "" + (Math.max(document.body.scrollTop, document.documentElement.scrollTop) / 2);
-    console.log("balls");
   } else if (Math.max(document.body.scrollTop, document.documentElement.scrollTop) > startThreshold * vh) {
     document.getElementById("header").style.fontSize = "" + startThreshold * vh / 2;
+  }
+  
+  // Hide Banner on scroll
+  if (Math.max(document.body.scrollTop, document.documentElement.scrollTop) > 110 * vh) {
+    document.getElementById("banner").style.display = 'none';
+  } else {
+    document.getElementById("banner").style.display = 'block';
   }
 }
